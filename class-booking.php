4d3a9f2cc82f7ea140dc48a41f8fbb5e80d689ca
Carlos <carlos@nectarestudio.com>
2026-02-01 19:03:08 +0100

    chore: Bump version to 0.2.0

--- class-booking.php
+++ class-booking.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Class Booking
  * Description: Custom booking system for classes with capacity control.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Your Name
  * Text Domain: class-booking
  */
@@ -11,7 +11,7 @@ defined('ABSPATH') || exit;
 
 define('CLASS_BOOKING_PATH', plugin_dir_path(__FILE__));
 define('CLASS_BOOKING_URL', plugin_dir_url(__FILE__));
-define('CLASS_BOOKING_VERSION', '0.1.0');
+define('CLASS_BOOKING_VERSION', '0.2.0');
 
 require_once CLASS_BOOKING_PATH . 'vendor/autoload.php';
 
